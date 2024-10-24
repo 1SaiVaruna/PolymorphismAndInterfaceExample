a87a75e0a78746fe9777ba7045986b7bfbdd1ae4
1SaiVaruna <sai030804@gmail.com>
2024-10-24 10:18:59 +0100

    Add project files.

---
+++ PolymorphismAndInterfaceExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymorphismAndInterfaceExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
