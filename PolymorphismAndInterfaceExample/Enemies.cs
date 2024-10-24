658123be24d72948e46ec88c2ce64a81f0d66351
1SaiVaruna <sai030804@gmail.com>
2024-10-24 10:25:25 +0100

    Added goblin class.

--- PolymorphismAndInterfaceExample/Enemies.cs
+++ PolymorphismAndInterfaceExample/Enemies.cs
@@ -1,4 +1,16 @@
 ﻿namespace PolymorphismAndInterfaceExample
 {
+    public class Goblin : IAttackable
+    {
+        public int Health { get; set; } = 10;
 
-}
+        public void Attacked (int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"Goblin hit for {damage} damage! Health: {Health}");
+            if (Health <= 0)
+            {
+                Console.WriteLine("Goblin defeated!");
+            }
+        }
+    }
